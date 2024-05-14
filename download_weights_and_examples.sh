73e1d54f51b0598ed5f9d5d34f849a92574f5058
Bernardo Vieira de Miranda <bernardo.miranda@smt.ufrj.br>
2024-05-14 20:44:39 -0300

    cleaning repo

--- download_weights_and_examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/eloimoliner/CQTdiff/releases/download/weights_and_examples/cqt_weights.pt
-mkdir experiments
-mkdir experiments/cqt
-mv cqt_weights.pt  experiments/cqt/
-
-wget https://github.com/eloimoliner/CQTdiff/releases/download/weights_and_examples/examples.zip
-unzip examples.zip 
